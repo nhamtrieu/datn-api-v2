9fb9b0fbaac5a43117d077a49b3f4d532419e4e2
ttnham <trieunham2002@gmail.com>
2024-12-13 02:00:00 +0700

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,16 @@ import { UserModule } from "./components/user/user.module";
 import { FirebaseModule } from "./components/firebase/firebase.module";
 import { DriverModule } from "./components/driver/driver.module";
 import { AuthModule } from "./components/auth/auth.module";
+import { LocationDto } from "./dto/location.dto";
 
 @Module({
-  imports: [TravelModule, UserModule, FirebaseModule, DriverModule, AuthModule],
+  imports: [
+    TravelModule,
+    UserModule,
+    FirebaseModule,
+    DriverModule,
+    AuthModule,
+    LocationDto,
+  ],
 })
 export class AppModule {}
